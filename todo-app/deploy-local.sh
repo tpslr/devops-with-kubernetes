480339dac35b8affa460d1cd507f606089d94b93
tpslr <topias.lyyra@gmail.com>
2025-09-19 13:23:57 +0000

    updated todo-app local deploy scripts

---
+++ todo-app/deploy-local.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -e
+
+echo "Building frontend docker image..."
+cd frontend
+npm run docker-build
+
+echo "Building backend docker image..."
+cd ../backend
+npm run docker-build
+
+cd ..
+
+echo "Importing images to kubernetes cluster..."
+k3d image import todo-app todo-app-backend -m direct
+
+echo "Deploying manifests..."
+kubectl apply -f manifests
