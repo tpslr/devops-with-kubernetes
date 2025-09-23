7a085abbc0332cb3bfe69b3262c4000eafd9ef25
tpslr <topias.lyyra@gmail.com>
2025-09-23 12:27:44 +0000

    excercise 2.10

--- todo-app/backend/src/index.ts
+++ todo-app/backend/src/index.ts
@@ -9,12 +9,16 @@ app.use(express.json());
 app.post("/api/todo", async (req, res) => {
     const todo = req.body;
     if (!todo || !isTodo(todo)) {
+        console.log(`(${req.ip}) Rejected invalid todo format`);
         return res.status(400).json({ message: "Invalid todo format" });
     }
 
     if (todo.title.length + todo.content.length > 120) {
+        console.log(`(${req.ip}) Rejected todo exceeding 120 characters: ${todo.title}, ${todo.content}`);
         return res.status(400).json({ message: "Todo is too long" });
     }
+
+    console.log(`(${req.ip}) Adding todo: ${todo.title}, ${todo.content}`);
 
     res.send(await addTodo(todo));
 });
